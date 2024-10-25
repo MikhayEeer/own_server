6c23b3403d350f08f1e00dcef4b0f7b2da6917f4
MikhayEeer <814416829@qq.com>
2024-10-25 15:34:07 +0800

    add main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include <server.h>
+
+int main(){
+    try {
+        Server server(8080);
+        server.start();
+    } catch (const std::exception& e){
+        std::cerr <<"Exception: " << e.what() <<std::endl;
+        return 1;
+    }
+    return 0;
+}
