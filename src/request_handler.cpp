78ad740d4d430d6bc54f62ce856a4cdb5c31a681
DoctorMango <dengzl@ios.ac.cn>
2025-01-19 18:18:32 +0800

    [front] add front

--- src/request_handler.cpp
+++ src/request_handler.cpp
@@ -5,18 +5,48 @@
 #include <iostream>
 
 void RequestHandler::handle_request(const std::string& request, boost::asio::ip::tcp::socket& socket) {
-    std::cout<< "Request received : " << request <<std::endl;
-    std::cout << "Handling request on socket " << socket.remote_endpoint() << std::endl;
-
-    std::string response_body = "Hello, This is a web server test.";
-    std::ostringstream response_stream;
-    response_stream << "HTTP/1.1 200 OK \r\n"
-                    << "Content-Length: " << response_body.size() << "\r\n"
-                    << "Content-Type: text/plain; charset=utf-8\r\n"
-                    << "\r\n"
-                    << response_body;
-    std::string response = response_stream.str();
-
-    boost::asio::write(socket, boost::asio::buffer(response));
-    std::cout << "Response sent to " << socket.remote_endpoint() << std::endl<<"=-=-=-="<<std::endl;
+    std::cout << "Request received : " << request << std::endl;
+    
+    // 解析请求方法和路径
+    std::string method = request.substr(0, request.find(' '));
+    std::string path = request.substr(request.find(' ') + 1);
+    path = path.substr(0, path.find(' '));
+    
+    if (method == "GET") {
+        // 处理GET请求，返回HTML页面
+        std::ifstream file("frontend/index.html");
+        std::stringstream buffer;
+        buffer << file.rdbuf();
+        std::string response_body = buffer.str();
+        
+        std::ostringstream response_stream;
+        response_stream << "HTTP/1.1 200 OK\r\n"
+                       << "Content-Length: " << response_body.size() << "\r\n"
+                       << "Content-Type: text/html; charset=utf-8\r\n"
+                       << "Access-Control-Allow-Origin: *\r\n"
+                       << "\r\n"
+                       << response_body;
+        
+        boost::asio::write(socket, boost::asio::buffer(response_stream.str()));
+    }
+    else if (method == "POST" && path == "/submit") {
+        // 从请求体中提取消息
+        size_t body_pos = request.find("\r\n\r\n");
+        std::string body = request.substr(body_pos + 4);
+        size_t msg_pos = body.find("message=");
+        std::string message = body.substr(msg_pos + 8);
+        
+        // 处理消息（这里简单地返回消息）
+        std::string response_body = "收到消息: " + message;
+        
+        std::ostringstream response_stream;
+        response_stream << "HTTP/1.1 200 OK\r\n"
+                       << "Content-Length: " << response_body.size() << "\r\n"
+                       << "Content-Type: text/plain; charset=utf-8\r\n"
+                       << "Access-Control-Allow-Origin: *\r\n"
+                       << "\r\n"
+                       << response_body;
+        
+        boost::asio::write(socket, boost::asio::buffer(response_stream.str()));
+    }
 }
